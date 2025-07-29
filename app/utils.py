718e710480f85afe95a21bc3cc9aa6ea0b9f153b
Ambuj Shrivastav <ambujshri4@gmail.com>
2025-07-29 19:41:28 +0530

    Create utils.py

---
+++ app/utils.py
@@ -0,0 +1,6 @@
+def clean_text(text):
+    import re
+    text = re.sub(r"http\S+|www\S+|https\S+", '', text, flags=re.MULTILINE)
+    text = re.sub(r'\@w+|\#','', text)
+    text = text.lower().strip()
+    return text
